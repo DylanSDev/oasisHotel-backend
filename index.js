ab3dac063a20546e3d0c7c28b4b1d54a959d742b
DylanSDev <dylansdiaz.dev@gmail.com>
2025-01-20 23:06:47 -0300

    Se configuró el puerto para el proyecto de backend.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("HOLA");
+import express from "express";
+
+//1 - Configuramos un puerto
+const app = express();
+
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.log("Estoy en el puerto: " + app.get("port"));
+});
+//2 - Configuramos middlewares
+
+//3 - Configuramos las rutas
