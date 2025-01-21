a1800f49c1a057d274e4ad15b67ed8a37f649403
DylanSDev <dylansdiaz.dev@gmail.com>
2025-01-20 23:23:50 -0300

    Se agrego cors y morgan, además probe una solicitud get.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
 
-//1 - Configuramos un puerto
 const app = express();
 
+//1 - Configuramos un puerto
 app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
   console.log("Estoy en el puerto: " + app.get("port"));
 });
+
 //2 - Configuramos middlewares
+app.use(cors()); //Obtenemos conexiones remotas
+app.use(morgan("dev")); //Me dará información extra en la terminal
+//Faltan agregar middlewares
 
 //3 - Configuramos las rutas
+app.get("/", (req, res) => {
+  console.log("procesando una solicitud get");
+  res.send("Respuesta del backend loquito");
+});
