deb3ae364fcf29e49e895cc71b8498b45d6f800e
DylanSDev <dylansdiaz.dev@gmail.com>
2025-01-23 12:50:43 -0300

    Se agregaron validaciones a las habitaciones con express-validator.

--- src/routes/habitaciones.routes.js
+++ src/routes/habitaciones.routes.js
@@ -5,6 +5,8 @@ import {
   eliminarHabitacion,
   editarHabitacion,
 } from "../controllers/habitaciones.controllers.js";
+import validacionEditarHabitacion from "../helpers/validacionEditarHabitacion.js";
+import validacionCrearHabitacion from "../helpers/validacionCrearHabitacion.js";
 import upload from "../middlewares/upload.js";
 
 const router = express.Router();
@@ -13,11 +15,11 @@ const router = express.Router();
 router
   .route("/habitaciones")
   .get(listarHabitaciones)
-  .post(upload.single("image"), crearHabitacion);
+  .post(upload.single("image"), validacionCrearHabitacion, crearHabitacion);
 
 router
   .route("/habitaciones/:id")
   .delete(eliminarHabitacion)
-  .put(upload.single("image"), editarHabitacion);
+  .put(upload.single("image"), validacionEditarHabitacion, editarHabitacion);
 
 export default router;
