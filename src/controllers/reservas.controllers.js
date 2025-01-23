3ac02563240686f58473e0dedf3076032af104f3
DylanSDev <dylansdiaz.dev@gmail.com>
2025-01-23 02:03:52 -0300

    Se agrego la peticion de listar reservas, y listar reservas por usuario.

--- src/controllers/reservas.controllers.js
+++ src/controllers/reservas.controllers.js
@@ -1,5 +1,37 @@
 import Reservas from "../databases/models/reservas.js";
 
-export const listarReservas = async (req, res) => {};
+//Lista todas las reservas
+export const listarReservas = async (req, res) => {
+  try {
+    // Obtenemos todas las reservas, populando los campos de usuario e habitación
+    const reservas = await Reservas.find()
+      .populate("idUser", "name email") // Especifica qué campos del usuario traer
+      .populate("idRoom", "number type price"); // Especifica qué campos de la habitación traer
+
+    // Devolvemos las reservas, que podrían ser un array vacío si no hay reservas
+    res.status(200).json(reservas);
+  } catch (error) {
+    // Si ocurre un error, devolvemos el error
+    res
+      .status(500)
+      .json({ message: "Error al obtener las reservas", error: error.message });
+  }
+};
+
+export const listarReservasPorUsuario = async (req, res) => {
+  const { idUser } = req.params; // Obtener el id del usuario desde los parámetros de la ruta
+  try {
+    const reservas = await Reservas.find({ idUser })
+      .populate("idUser", "name email")
+      .populate("idRoom", "number type price");
+
+    // Si no hay reservas, se devuelve un array vacío
+    res.status(200).json(reservas);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error al obtener las reservas", error: error.message });
+  }
+};
 
 export const crearReserva = async (req, res) => {};
--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
   listarReservas,
+  listarReservasPorUsuario,
   crearReserva,
 } from "../controllers/reservas.controllers.js";
 const router = express.Router();
 
 // Rutas para reservas
 router.route("/reservas").get(listarReservas).post(crearReserva);
+router.route("/reservas/usuario/:idUser").get(listarReservasPorUsuario);
 
 export default router;
